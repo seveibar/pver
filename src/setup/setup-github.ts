c4744cd7fb056d26a2430465da66c802201751d0
seveibar <seveibar@gmail.com>
2024-01-15 14:50:45 -0800

    increment version, more setup comment details

--- src/setup/setup-github.ts
+++ src/setup/setup-github.ts
@@ -3,7 +3,8 @@ import { AppContext } from "../app-context"
 /**
  * Setup github parts:
  * - Create github action
- * - Make sure secret is set for repo
+ * - Make sure NODE_AUTH_TOKEN secret is set for repo and the action has
+ *   registry-url set
  * - Make sure https://github.com/seveibar/pver/settings/actions says that
  *   the action is allow read and write permissions
  */
