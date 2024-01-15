9bb4b30a722d1b906cba89f6bf660e5aa4c43790
seveibar <seveibar@gmail.com>
2024-01-14 22:36:02 -0800

    npm publish fixes, throw error and inherit env

--- src/release/npm-publish.ts
+++ src/release/npm-publish.ts
@@ -1,8 +1,13 @@
 import { spawnSync } from "child_process"
 import { AppContext } from "../app-context"
 export const npmPublish = (ctx: AppContext) => {
-  spawnSync("npm", ["publish"], {
+  const result = spawnSync("npm", ["publish"], {
     cwd: ctx.current_directory,
+    env: process.env,
     stdio: "inherit",
   })
+
+  if (result.status !== 0) {
+    throw new Error("npm publish failed")
+  }
 }
