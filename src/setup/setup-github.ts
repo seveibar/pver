904d2be5ddef59d5f2865346a5f8eb81df6961c5
seveibar <seveibar@gmail.com>
2024-01-15 14:49:30 -0800

    allude to setup

---
+++ src/setup/setup-github.ts
@@ -0,0 +1,10 @@
+import { AppContext } from "../app-context"
+
+/**
+ * Setup github parts:
+ * - Create github action
+ * - Make sure secret is set for repo
+ * - Make sure https://github.com/seveibar/pver/settings/actions says that
+ *   the action is allow read and write permissions
+ */
+export const setupGithub = async (ctx: AppContext) => {}
