30962508d77913f79f38052d307a5fb7d0e8225e
seveibar <seveibar@gmail.com>
2024-02-02 18:19:49 -0800

    pull with rebase

--- src/release/push-to-main.ts
+++ src/release/push-to-main.ts
@@ -4,5 +4,10 @@ import { AppContext } from "../app-context"
 export const pushToMain = async (ctx: AppContext) => {
   console.log("Pushing to main")
   const git = simpleGit(ctx.current_directory)
+
+  // sometimes when pushing to main we get an error that we need to integrate
+  // remote changes first, let's pull main and rebase
+  await git.pull("origin", "main", ["--rebase"])
+
   await git.push(["origin", `HEAD:${process.env.GITHUB_REF ?? "main"}`])
 }
