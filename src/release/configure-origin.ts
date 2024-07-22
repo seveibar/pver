0d62245ef986783257d45f9004697149c0677d43
seveibar <seveibar@gmail.com>
2024-07-21 19:39:17 -0700

    remove origin configuration

--- src/release/configure-origin.ts
+++ src/release/configure-origin.ts
@@ -1,13 +1,15 @@
 export const configureOrigin = async (git: any) => {
   if (process.env.GITHUB_TOKEN) {
-    const remote_url = await git.remote(["get-url", "origin"])
-    if (!remote_url.includes("x-access-token:")) {
-      const new_url = remote_url!.replace(
-        "https://",
-        `https://x-access-token:${process.env.GITHUB_TOKEN}@`
-      )
-      await git.removeRemote("origin")
-      await git.addRemote("origin", new_url)
-    }
+    // This doesn't work- it's possible the checkout command needs to use the
+    // github token (in the workflow yaml)
+    // const remote_url = await git.remote(["get-url", "origin"])
+    // if (!remote_url.includes("x-access-token:")) {
+    //   const new_url = remote_url!.replace(
+    //     "https://",
+    //     `https://x-access-token:${process.env.GITHUB_TOKEN}@`
+    //   )
+    //   await git.removeRemote("origin")
+    //   await git.addRemote("origin", new_url)
+    // }
   }
 }
