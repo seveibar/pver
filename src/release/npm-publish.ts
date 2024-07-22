39a422695ada52cad95b3695025a9ea3805ff639
seveibar <seveibar@gmail.com>
2024-07-21 20:24:43 -0700

    additional handling for NPM_TOKEN

--- src/release/npm-publish.ts
+++ src/release/npm-publish.ts
@@ -3,7 +3,10 @@ import { AppContext } from "../app-context"
 export const npmPublish = (ctx: AppContext) => {
   const result = spawnSync("npm", ["publish"], {
     cwd: ctx.current_directory,
-    env: process.env,
+    env: {
+      ...process.env,
+      NODE_AUTH_TOKEN: process.env.NODE_AUTH_TOKEN || process.env.NPM_TOKEN,
+    },
     stdio: "inherit",
   })
 
