e96afcbc8c3caf48cf0832bbbc6e8519f7a6f0eb
seveibar <seveibar@gmail.com>
2024-07-21 20:07:23 -0700

    try publishing with node auth token

--- src/release/configure-origin.ts
+++ src/release/configure-origin.ts
@@ -2,14 +2,23 @@ export const configureOrigin = async (git: any) => {
   if (process.env.GITHUB_TOKEN) {
     // This doesn't work- it's possible the checkout command needs to use the
     // github token (in the workflow yaml)
-    // const remote_url = await git.remote(["get-url", "origin"])
-    // if (!remote_url.includes("x-access-token:")) {
-    //   const new_url = remote_url!.replace(
-    //     "https://",
-    //     `https://x-access-token:${process.env.GITHUB_TOKEN}@`
-    //   )
-    //   await git.removeRemote("origin")
-    //   await git.addRemote("origin", new_url)
-    // }
+    const remote_url = await git.remote(["get-url", "origin"])
+
+    // if the remote is in the form of git@github.com:foo/bar.git, convert it to
+    // https://github.com/foo/bar.git
+    if (remote_url.includes("git@")) {
+      const new_url = remote_url!.replace("git@", "https://")
+      await git.removeRemote("origin")
+      await git.addRemote("origin", new_url)
+    }
+
+    if (!remote_url.includes("oauth2:")) {
+      const new_url = remote_url!.replace(
+        "https://",
+        `https://oauth2:${process.env.GITHUB_TOKEN}@`
+      )
+      await git.removeRemote("origin")
+      await git.addRemote("origin", new_url)
+    }
   }
 }
